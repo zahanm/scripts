9bebf4c0306449cc799158ff37fba792d833d473
Zahan Malkani <zahanm@gmail.com>
2020-11-28 12:43:16 -0800

    Make the server a background process
    Detach it, and kill it on the next run.
    Send logs to a file.

--- lib/updateFavaServer.ts
+++ lib/updateFavaServer.ts
@@ -1,5 +1,5 @@
 import { execSync, spawn } from "child_process";
-import { mkdir, writeFile } from "fs/promises";
+import { mkdir, open, readFile, writeFile } from "fs/promises";
 import * as path from "path";
 
 import { pathExists } from "./utils";
@@ -14,9 +14,36 @@ export async function updateFavaServer(
   if (!path.isAbsolute(repo)) {
     throw new Error(`Must provide absolute path for repo: ${repo}.`);
   }
+  const userId = execSync("id --user").toString().trim();
+  const pidile = `/tmp/fava-server-${userId}/server.pid`;
+  const dataDir = path.dirname(pidile);
+  let oldPid: number | null = null;
+  try {
+    oldPid = parseInt((await readFile(pidile)).toString());
+  } catch (e) {
+    throw e;
+  }
+  if (oldPid != null) {
+    console.error(`kill ${oldPid}`);
+    try {
+      process.kill(oldPid);
+    } catch (e) {
+      if (e.code === "EPERM") throw e;
+      // otherwise, the process doesn't exist - ie, it's not running
+      console.error("Old server died.");
+    }
+  }
   console.error(
     `Running: pipenv run fava personal.beancount --port 8080 --host=0.0.0.0`
   );
+  try {
+    await mkdir(dataDir);
+  } catch (e) {
+    // Handle the case where the directory already exists
+    if (e.code != "EEXIST") throw e;
+  }
+  const out = await open(path.join(dataDir, "out.log"), "a");
+  const err = await open(path.join(dataDir, "err.log"), "a");
   const fava = spawn(
     "pipenv",
     [
@@ -30,18 +57,12 @@ export async function updateFavaServer(
     ],
     {
       cwd: repo,
-      // detached: true,
+      detached: true,
+      stdio: ["ignore", out.fd, err.fd],
     }
   );
+  fava.unref();
   console.log(`PID: ${fava.pid}`);
-  const userId = execSync("id --user").toString().trim();
-  const pidile = `/tmp/fava-server-${userId}/server.pid`;
   console.error(`Writing to: ${pidile}`);
-  try {
-    await mkdir(path.dirname(pidile));
-  } catch (err) {
-    // Handle the case where the directory already exists
-    if (err.code != "EEXIST") throw err;
-  }
   await writeFile(pidile, fava.pid.toString());
 }
