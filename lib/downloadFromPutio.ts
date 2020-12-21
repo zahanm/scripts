835756d7e9e6a23233b6b4f47062bab62505e0f8
Zahan Malkani <zahanm@fb.com>
2020-12-20 19:29:39 -0800

    Get root dir contents from Put.io, and find item

--- lib/downloadFromPutio.ts
+++ lib/downloadFromPutio.ts
@@ -1,14 +1,16 @@
-import { open, readFile, stat } from "fs/promises";
+import { stat } from "fs/promises";
 import { URL } from "url";
+import { spawnSync } from "child_process";
 
 import * as Parser from "rss-parser";
+import escapeStringRegexp = require("escape-string-regexp");
 
 /**
  * This is going to use Rclone as the interface to Put.io. That means it needs to be set up locally as a prerequisite.
  * 1. Fetch showRSS.info feed
  * 2. Check if there are items newer than the last download
- * 3. Look for that item in Put.io, see if it's there yet
- * 3. Download that item using Rclone, and put it in the right folder
+ * 3. Look for each item in Put.io, see if it's there yet
+ * 4. Download that item using Rclone, and put it in the right folder
  */
 export async function downloadFromPutio(
   opts: Record<string, any>,
@@ -17,11 +19,19 @@ export async function downloadFromPutio(
 ) {
   checkArgs(feedUrl);
   const allItems = await fetchShowRssFeed(feedUrl);
+  console.error(`Got ${allItems.length} items in the feed.`);
   const lastDownload = await getLastDownloadTime(downloadTsFile);
   const newItems = itemsNewerThan(allItems, lastDownload);
-  newItems.forEach((item) => {
-    console.log(`${item.title} -> ${item.link}`);
-  });
+  console.error(`${newItems.length} new items to download.`);
+  const putioEntries = await lsPutio();
+  for (const item of newItems.slice(0, 1)) {
+    const entry = findPutioEntry(putioEntries, item);
+    if (entry) {
+      console.error(`Found ${entry.Path} ${entry.IsDir}.`);
+    } else {
+      console.error(`Could not find ${item.title}.`);
+    }
+  }
 }
 
 function checkArgs(url: string) {
@@ -33,9 +43,12 @@ type TvItem = { "tv:show_name": string; "tv:raw_title": string };
 type Item = Parser.Item & TvItem;
 
 async function fetchShowRssFeed(feedURL: string): Promise<Item[]> {
-  const parser = new Parser<TvFeed, TvItem>();
+  const parser = new Parser<TvFeed, TvItem>({
+    customFields: {
+      item: ["tv:show_name", "tv:raw_title"],
+    },
+  });
   const feed = await parser.parseURL(feedURL);
-  console.log(`Got ${feed.items.length} items in the feed`);
   return feed.items;
 }
 
@@ -62,3 +75,31 @@ function itemsNewerThan(items: Item[], ts: Date): Item[] {
     }
   });
 }
+
+type PutioEntry = {
+  Path: string;
+  Name: string;
+  IsDir: boolean;
+  ModTime: string;
+};
+
+async function lsPutio(): Promise<PutioEntry[]> {
+  const { stdout } = spawnSync("rclone", ["lsjson", "putio:"]);
+  const out: PutioEntry[] = JSON.parse(stdout);
+  return out;
+}
+
+function findPutioEntry(
+  putioEntries: PutioEntry[],
+  item: Item
+): PutioEntry | undefined {
+  const words = item["tv:raw_title"].split(/\s/);
+  const needle = new RegExp(
+    words.map((word) => escapeStringRegexp(word)).join(".+")
+  );
+  return putioEntries.find((entry) => {
+    return !!needle.exec(entry.Name);
+  });
+}
+
+async function downloadItem() {}
