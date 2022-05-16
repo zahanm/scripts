7a088baf918db5c50ba4d60501e993617bfa97de
Zahan Malkani <zahanm@fb.com>
2022-05-15 23:13:49 -0700

    Added a --dry-run option

--- python/downloadFromPutio.py
+++ python/downloadFromPutio.py
@@ -11,6 +11,7 @@ import json
 import subprocess
 import os.path as ospath
 import readline  # needed for input() to give more keyboard control
+import argparse
 
 blocklist = {"chill.institute"}
 putio_rclone_mount = "putio"
@@ -18,63 +19,81 @@ media_root = "/volume1/media"
 
 
 def run():
+    argv = parse_args()
     check_rclone_installed()
-    items = list_items("")
+    runner = Downloader(argv)
+    items = runner.list_items("")
     print(f"Found {len(items)} items")
     for item in items:
         if item["Name"] not in blocklist:
-            process_item(item)
+            runner.process_item(item)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        help="do not take any actions, just show what would happen",
+    )
+    return parser.parse_args()
 
 
 def check_rclone_installed():
     subprocess.run(["which", "rclone"], check=True, stdout=subprocess.DEVNULL)
 
 
-def list_items(path):
-    proc = subprocess.run(
-        ["rclone", "lsjson", f"{putio_rclone_mount}:{path}"],
-        check=True,
-        capture_output=True,
-        text=True,
-    )
-    return json.loads(proc.stdout)
+class Downloader:
+    def __init__(self, argv) -> None:
+        self.argv = argv
 
+    def list_items(self, path):
+        proc = subprocess.run(
+            ["rclone", "lsjson", f"{putio_rclone_mount}:{path}"],
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        return json.loads(proc.stdout)
 
-def process_item(item):
-    print()
-    print(item["Name"])
-    if not item["IsDir"] and is_video_mimetype(item["MimeType"]):
-        offer_download(item["Name"], item["Path"], item["Size"])
-    else:
-        subitems = list_items(item["Path"])
-        videos = [it for it in subitems if is_video_mimetype(it["MimeType"])]
-        if len(videos) > 1:
-            print("Mutiple videos!")
-        elif len(videos) == 0:
-            print("No video!")
+    def process_item(self, item):
+        print()
+        print(item["Name"])
+        if not item["IsDir"] and is_video_mimetype(item["MimeType"]):
+            self.offer_download(item["Name"], item["Path"], item["Size"])
         else:
-            video = videos[0]
-            offer_download(
-                item["Name"], ospath.join(item["Path"], video["Path"]), video["Size"]
-            )
+            subitems = self.list_items(item["Path"])
+            videos = [it for it in subitems if is_video_mimetype(it["MimeType"])]
+            if len(videos) > 1:
+                print("Mutiple videos!")
+            elif len(videos) == 0:
+                print("No video!")
+            else:
+                video = videos[0]
+                self.offer_download(
+                    item["Name"],
+                    ospath.join(item["Path"], video["Path"]),
+                    video["Size"],
+                )
 
-
-def offer_download(name, path, size):
-    answer = input(f"{human_readable_size(size)} | Download (y/n)?: ")
-    if answer.lower() == "y":
-        movie_name = input(f"Movie name?: ")
-        assert len(movie_name) > 0
-        dest = ospath.join(media_root, "Movies", movie_name)
-        subprocess.run(
-            [
+    def offer_download(self, name, path, size):
+        answer = input(f"{human_readable_size(size)} | Download (y/n)?: ")
+        if answer.lower() == "y":
+            movie_name = input(f"Movie name?: ")
+            assert len(movie_name) > 0
+            dest = ospath.join(media_root, "Movies", movie_name)
+            call_args = [
                 "rclone",
                 "copy",
                 "--progress",
                 f"{putio_rclone_mount}:{path}",
                 f"{dest}",
-            ],
-            check=True,
-        )
+            ]
+            if self.argv.dry_run:
+                print("(dry-run)", " ".join(call_args))
+            else:
+                subprocess.run(call_args, check=True)
 
 
 def is_video_mimetype(mtype: str):
